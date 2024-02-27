d8d31e82fad4ec525ccf909f64740c678e8bb65e
Viswa_011 <102846076+Viswanath011@users.noreply.github.com>
2024-02-27 10:34:45 +0530

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
